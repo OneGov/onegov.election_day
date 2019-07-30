4c0e8de9a5f5ad8037ddac7a25fcb1c5cd3bb6d4
Lukas <lukas.burkhard@seantis.ch>
2019-07-30 17:28:49 +0200

    Add test that domain of influences are coherent with ballot app

--- onegov/election_day/tests/models/test_archived_result.py
+++ onegov/election_day/tests/models/test_archived_result.py
@@ -1,6 +1,8 @@
 from datetime import date
 from datetime import datetime
 from datetime import timezone
+
+from onegov.ballot.models.mixins import DomainOfInfluenceMixin
 from onegov.election_day.models import ArchivedResult
 from onegov.election_day.tests.common import DummyRequest
 
@@ -212,3 +214,7 @@ def test_archived_result_local_results(session):
     assert result.display_answer(request) == 'rejected'
     assert result.display_nays_percentage(request) == 60.0
     assert result.display_yeas_percentage(request) == 40.0
+
+def test_domain_types():
+    domain_entries = [d[0] for d in ArchivedResult.types_of_domains]
+    assert domain_entries == DomainOfInfluenceMixin.allowed_domains
