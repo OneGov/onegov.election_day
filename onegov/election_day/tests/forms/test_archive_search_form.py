df0c20f43a159f2648800a5cb744c095393e1f43
Lukas <lukas.burkhard@seantis.ch>
2019-08-05 16:43:43 +0200

    Adds tests form archive search form

--- onegov/election_day/tests/forms/test_archive_search_form.py
+++ onegov/election_day/tests/forms/test_archive_search_form.py
@@ -1,6 +1,24 @@
-# from onegov.election_day.forms import ArchiveSearchForm
+from onegov.election_day.collections import SearchableArchivedResultCollection
+from onegov.election_day.forms import ArchiveSearchForm
+from onegov.election_day.tests.common import DummyRequest
+from datetime import date
 
+def test_apply_model_archive_search_form(session):
+    archive = SearchableArchivedResultCollection(session)
+    archive.term = 'xxx'
+    archive.from_date = date(2222, 1, 1)
+    archive.to_date = date(2222, 1, 1)
+    archive.answer = ['accepted']
+    archive.type = ['election', 'vote']
+    archive.domain = ['region', 'municipality']
 
-def test_default_inputs():
-    # form = ArchiveSearchForm()
-    pass
+    form = ArchiveSearchForm()
+    # form.request = DummyRequest()
+    form.apply_model(archive)
+    assert form.term.data == archive.term
+    assert form.from_date.data == archive.from_date
+    assert form.to_date.data == archive.to_date
+    assert form.answer.data == archive.answer
+    assert form.type.data == archive.type
+    assert form.domain == archive.domain
+
